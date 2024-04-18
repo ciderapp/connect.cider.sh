758a95f7508430c9faf73aa587d17c32e60a0251
elliotjarnit <me@elliotjarnit.dev>
2024-04-18 13:14:31 -0700

    Why nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import materialIcons from "quasar/icon-set/svg-material-icons";
+import roundedMaterialIcons from "quasar/icon-set/svg-material-icons-round";
+import fontAwesome from "quasar/icon-set/fontawesome-v6";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
@@ -12,5 +16,33 @@ export default defineNuxtConfig({
     redirectURL: process.env.NUXT_KINDE_REDIRECT_URL,
     logoutRedirectURL: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
     postLoginRedirectURL: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
+  },
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    },
+  },
+  quasar: {
+    config: {
+      dark: true
+    },
+    sassVariables: "~/assets/quasar-variables.scss",
+    iconSet: {
+      ...materialIcons,
+      ...roundedMaterialIcons,
+      ...fontAwesome,
+    },
+    extras: {
+      fontIcons: ["material-icons", "fontawesome-v6", "material-icons-round"],
+    },
+    plugins: ["Dialog", "Notify"],
+  },
+  vite: {
+    vue: {
+      script: {
+        defineModel: true,
+        propsDestructure: true,
+      },
+    },
   },
 })
